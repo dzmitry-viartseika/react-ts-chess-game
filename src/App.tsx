942e1601c63f3876822c9a316c872e0e77fbfae8
Dmitry Verteyko <verteyko1990@gmail.com>
2022-05-18 22:21:35 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect, useReducer} from 'react';
+
+
+function App(): JSX.Element {
+
+  return (
+    <div className={'App'}>
+              wertey
+    </div>
+  );
+}
+
+export default App;
